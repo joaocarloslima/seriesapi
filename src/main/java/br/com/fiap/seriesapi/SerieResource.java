4e7c232ec44a6c73134b1c108782546c75b4b7f6
Logon PF Fiap <logonpflocal@fiap.com.br>
2023-10-24 20:34:45 -0300

    post, delete e post

--- src/main/java/br/com/fiap/seriesapi/SerieResource.java
+++ src/main/java/br/com/fiap/seriesapi/SerieResource.java
@@ -3,7 +3,10 @@ package br.com.fiap.seriesapi;
 import java.util.List;
 
 import br.com.fiap.seriesapi.model.Serie;
+import jakarta.ws.rs.DELETE;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.PUT;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
@@ -35,6 +38,47 @@ public class SerieResource {
     	
     	return Response.ok(serie).build();
     }
+    
+    @DELETE
+    @Path("{id}")
+    public Response delete(@PathParam("id") Long id) {
+    	var serie = service.findById(id);
+    	
+    	if(serie == null)
+    		return Response.status(404).build();
+    	
+    	service.delete(serie);
+    	return Response.status(Response.Status.NO_CONTENT).build();
+    }
+    
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response cadastrar(Serie serie){
+    	if (!service.save(serie)) {
+    		return Response.status(Response.Status.BAD_REQUEST).build();
+    	}
+    	
+    	return Response.ok(serie).build();
+    	
+    }
+    
+    @PUT
+    @Path("{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response atualizar(@PathParam("id") Long id, Serie serie) {
+    	var serieEncontrada = service.findById(id);
+    	
+    	if(serieEncontrada == null)
+    		return Response.status(Response.Status.NOT_FOUND).build();
+    			
+    			
+    	if (!service.update(serie))
+    		return Response.status(Response.Status.BAD_REQUEST).build();
+    	
+    	return Response.ok(serie).build();
+    	
+    }
+    
     
     
     
--- src/main/java/service/SerieService.java
+++ src/main/java/service/SerieService.java
@@ -18,4 +18,42 @@ public class SerieService {
 		return dao.findById(id);
 	}
 
+	public void delete(Serie serie) {
+		// apagar do BD
+		
+	}
+
+	public boolean save(Serie serie) {
+		//validação
+		if (serie.id() == 0) return false;
+		if (serie.titulo().length() < 5) return false;
+		if (serie.sinopse().length() < 10) return false;
+		
+		//inserir no BD
+		return true;
+		
+	}
+
+	public boolean update(Serie serie) {
+		// atualizar no BD
+		return true;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
 }
